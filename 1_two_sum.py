28cbf459670c29a2e3d788b0322b7e6a0e161b40
Jaya Vardhan Swarna <79640790+Jayavardhan-swarna@users.noreply.github.com>
2023-06-06 11:09:46 +0530

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,14 +1,21 @@
 def two_sum(nums, target):
+    # Create a dictionary to store the complement of each number
     complement_dict = {}
 
+    # Iterate through the array
     for i, num in enumerate(nums):
+        # Calculate the complement
         complement = target - num
 
+        # Check if the complement exists in the dictionary
         if complement in complement_dict:
+            # Return the indices of the current number and its complement
             return [complement_dict[complement], i]
 
+        # Add the current number and its index to the dictionary
         complement_dict[num] = i
 
+    # If no solution is found, return an empty list or raise an exception
     return []
 
 
