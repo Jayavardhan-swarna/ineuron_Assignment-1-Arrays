7b09df9a90153069b99375ac9915f141f894f07b
Jaya Vardhan Swarna <79640790+Jayavardhan-swarna@users.noreply.github.com>
2023-06-06 11:07:24 +0530

    Create 1_two_sum.py

---
+++ 1_two_sum.py
@@ -0,0 +1,18 @@
+def two_sum(nums, target):
+    complement_dict = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in complement_dict:
+            return [complement_dict[complement], i]
+
+        complement_dict[num] = i
+
+    return []
+
+
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print(result)  # Output: [0, 1] 
